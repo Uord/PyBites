f421dd60401ee4929d9055eda17123e130e97596
Dawid <dzakrzewski878@gmail.com>
2022-07-28 17:04:14 +0200

    Add Bite 95

---
+++ Bite 95/bdaydict.py
@@ -0,0 +1,12 @@
+MSG = "Hey {}, there are more people with your birthday!"
+
+
+class BirthdayDict(dict):
+    """Override dict to print a message every time a new person is added that has
+    the same birthday (day+month) as somebody already in the dict"""
+
+    def __setitem__(self, name, birthday):
+        for bd in super().values():
+            if bd.day == birthday.day and bd.month == birthday.month:
+                print(MSG.format(name))
+        super().__setitem__(name, birthday)
