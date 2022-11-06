1ee3dadbbb24535d5537fb5f153add1b8bbada14
Uord <dzakrzewski878@gmail.com>
2022-11-06 10:18:21 +0100

    Bite 11 Solution

---
+++ Bite011/account.py
@@ -0,0 +1,39 @@
+from functools import total_ordering
+
+@total_ordering
+class Account:
+
+    def __init__(self, name, start_balance=0):
+        self.name = name
+        self.start_balance = start_balance
+        self._transactions = []
+
+    @property
+    def balance(self):
+        return self.start_balance + sum(self._transactions)
+
+    def __len__(self):
+        return len(self._transactions)
+    
+    def __getitem__(self, position):
+        return self._transactions[position]
+    
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+        
+    def __add__(self, amount):
+        if not isinstance(amount, int):
+            raise TypeError 
+        self._transactions.append(amount)
+        
+    def __sub__(self, amount):
+        if not isinstance(amount, int):
+            raise TypeError 
+        self._transactions.append(-amount)
+    
+    def __str__(self):
+        return f'{self.name} account - balance: {self.balance}'
+    
