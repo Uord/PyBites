1f1adf358681b71831ad59c2fec5d0d8e0226f49
Uord <dzakrzewski878@gmail.com>
2022-11-06 10:10:13 +0100

    Bite 4 Solution

---
+++ Bite004/tags.py
@@ -0,0 +1,30 @@
+import os
+from collections import Counter
+import urllib.request
+import xml.etree.ElementTree as ET
+
+# prep
+tmp = os.getenv("TMP", "/tmp")
+tempfile = os.path.join(tmp, 'feed')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/feed',
+    tempfile
+)
+
+with open(tempfile) as f:
+    content = f.read().lower()
+
+
+def get_pybites_top_tags(n=10):
+    """use Counter to get the top 10 PyBites tags from the feed
+       data already loaded into the content variable"""
+    root = ET.fromstring(content)
+    top_tags = []
+    channels = root.findall('channel')
+    for channel in channels:
+        items = channel.findall('item')
+        for item in items:
+            categories = item.findall('category')
+            for category in categories:
+                top_tags.append(category.text)
+    return Counter(top_tags).most_common(n)
